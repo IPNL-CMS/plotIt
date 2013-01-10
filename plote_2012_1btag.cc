75d739ad3d561433ac3d771e53ef25a9e6f9423d
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2013-01-10 10:25:27 +0100

    [PlotIt] changed: added plots for 1 & 2 btags

---
+++ plote_2012_1btag.cc
@@ -0,0 +1,44 @@
+{
+  gROOT->SetBatch();
+  gSystem->Load("PlotIt_cc");
+
+  PlotIt p("semie2012_1btag.list", 1);
+
+  p.plotstack_ratio("hNGoodJets");
+  c1.SaveAs("plots/1btag/nJets_semie.pdf");
+
+  p.plotstack_ratio("hNBtaggedJets", 1, 1, "Number of b-tagged jets");
+  c1.SaveAs("plots/1btag/nBJets_semie.pdf");
+
+  p.plotstack_ratio("hElRelIso", 1, 1, "Electron relative isolation");
+  c1.SaveAs("plots/1btag/Electron_relative_iso.pdf");
+
+  p.plotstack_ratio("hmttSelected_btag_sel", 5);
+  c1.SaveAs("plots/1btag/mtt_2btag_semie.pdf");
+
+  p.plotstack_ratio("hLeptonPt", 4);
+  c1.SaveAs("plots/1btag/Electron_pt.pdf");
+
+  p.plotstack_ratio("h1stjetpt", 4);
+  c1.SaveAs("plots/1btag/firstjet_semie.pdf");
+
+  p.plotstack_ratio("h2ndjetpt", 4);
+  c1.SaveAs("plots/1btag/secondjet_semie.pdf");
+
+  p.plotstack_ratio("h3rdjetpt", 4);
+  c1.SaveAs("plots/1btag/thirdjet_semie.pdf");
+
+  p.plotstack_ratio("h4thjetpt", 4, 1, "4^{th} jet p_{T} [GeV/c]");
+  c1.SaveAs("plots/1btag/fourthjet_semie.pdf");
+
+  p.plotstack_ratio("hMET", 4);
+  c1.SaveAs("plots/1btag/MET_semie.pdf");
+
+  p.plotstack_ratio("hNVtx", 2);
+  c1.SaveAs("plots/1btag/nvertex_semie.pdf");
+
+  p.plotstack_ratio("hNVtx", 2);
+  c1.SaveAs("plots/1btag/nvertex_ratio_semie.pdf");
+
+  exit(0);
+}
